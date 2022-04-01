40a380d9d8e1d4a51b782c77a37add600079dd01
accfmac95 <mjberry4@asu.edu>
2022-03-31 22:53:27 -0700

    Updated GUI

---
+++ SignInController.java
@@ -0,0 +1,11 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class SignInController {
+
+    @FXML
+    void verifyLogin(ActionEvent event) {
+        System.out.print("Hello");
+    }
+
+}
---
+++ out/production/CSE-360-Project/README.md
@@ -0,0 +1,6 @@
+# CSE-360-Project
+# hello team - Brody
+# hello team - Max
+# hello team - Adam
+# hello team - Andre
+# hello team - Sahil
