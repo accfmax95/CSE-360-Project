afe828aef6fd285dbfd744aa194cffa653b19396
accfmac95 <mjberry4@asu.edu>
2022-03-31 23:15:59 -0700

    Updated GUI

--- SignInController.java
+++ SignInController.java
@@ -1,11 +1,24 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class SignInController {
 
     @FXML
+    private PasswordField passInput;
+
+    @FXML
+    private TextField userInput;
+
+    @FXML
+    void signUp(ActionEvent event) {
+
+    }
+
+    @FXML
     void verifyLogin(ActionEvent event) {
-        System.out.print("Hello");
+
     }
 
 }
