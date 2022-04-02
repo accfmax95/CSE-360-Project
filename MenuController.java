fb94d95aa16979b14a2bdf489d23194c9fedb5f9
accfmac95 <mjberry4@asu.edu>
2022-04-01 19:19:29 -0700

    Added Menu

--- MenuController.java
+++ MenuController.java
@@ -1,7 +1,15 @@
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class MenuController {
 
@@ -19,6 +27,18 @@ public class MenuController {
     @FXML
     void plusButtonIcon(MouseEvent event) {
 
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("AddItem.fxml")));
+
+            Stage primaryStage = new Stage();
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.initModality(Modality.NONE);
+            primaryStage.show();
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
     }
 
     @FXML
@@ -29,6 +49,18 @@ public class MenuController {
     @FXML
     void viewOptionsForThreeLines(MouseEvent event) {
 
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Profile.fxml")));
+
+            Stage primaryStage = new Stage();
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.initModality(Modality.NONE);
+            primaryStage.show();
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
     }
 
 }
