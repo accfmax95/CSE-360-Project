f3976cd02e92c5a9508d31920d65680befca9ad1
accfmac95 <mjberry4@asu.edu>
2022-04-01 19:33:33 -0700

    Added Menu

---
+++ RemoveItemController.java
@@ -0,0 +1,35 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class RemoveItemController {
+
+    @FXML
+    private TextField itemName;
+
+    @FXML
+    void returnToMenu(ActionEvent event) {
+
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Menu.fxml")));
+
+            Stage primaryStage = new Stage();
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.initModality(Modality.NONE);
+            primaryStage.show();
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+}
