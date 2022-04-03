c9902d123fb2e1c2b68e43df8a6372176dac9c0a
accfmac95 <mjberry4@asu.edu>
2022-04-03 16:03:04 -0700

    Added the text

--- MenuController.java
+++ MenuController.java
@@ -1,33 +1,33 @@
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.text.Text;
-import javafx.stage.Modality;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-import javafx.scene.control.Label;
-import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
-import java.util.Scanner;
+import java.util.ResourceBundle;
 
-public class MenuController extends ProfileController{
+public class MenuController implements Initializable {
 
     @FXML
-    private ScrollPane menuPane;
+    private GridPane gridPane;
+
+    @FXML
+    private ScrollPane menuScrollPane;
 
     @FXML
     private TextField searchedItem;
 
     @FXML
     void accessCart(MouseEvent event) {
-
         try {
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Cart.fxml")));
 
@@ -66,22 +66,26 @@ public class MenuController extends ProfileController{
 
     @FXML
     void viewOptionsForThreeLines(MouseEvent event) {
-
         try {
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Profile.fxml")));
 
             Stage primaryStage = new Stage();
             primaryStage = (Stage)((Node)event.getSource()).getScene().getWindow();
-            
+
             Scene scene = new Scene(root);
             primaryStage.setScene(scene);
             primaryStage.show();
-            
-            
+
+
         } catch (IOException e) {
 
             e.printStackTrace();
         }
     }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
+
